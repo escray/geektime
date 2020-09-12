2dabd6fd278668ff9672393891589d5c4657bd89
escray <zescray@gmail.com>
2020-09-12 23:06:59 +0800

    DemoServer

---
+++ JavaCamp/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+  public static void print(){
+    System.out.println("Hello World");
+  }
+}
