64d7def519e75fbec0c2ccfa94a9403799cfbafd
escray <zescray@gmail.com>
2020-09-24 14:29:55 +0800

    Observer Pattern

---
+++ BackendInterview/Phone.java
@@ -0,0 +1,71 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class Phone {
+    private Dialer dialer;
+    private Button[] digitButtons;
+    private Button sendButton;
+
+    public Phone() {
+        dialer = new Dialer();
+        digitButtons = new Button[10];
+        for (int i = 0; i < digitButtons.length; i++) {
+            digitButtons[i] = new Button();
+            final int digit = i;
+            digitButtons[i].addListener(new ButtonListener(){
+                public void buttonPressed() {
+                    dialer.enterDigit(digit);
+                }
+            });
+        }
+        sendButton = new Button();
+        sendButton.addListener(new ButtonListener() {
+            public void buttonPressed() {
+                dialer.dial();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        phone.digitButtons[9].press();
+        phone.digitButtons[1].press();
+        phone.digitButtons[1].press();
+        phone.sendButton.press();
+    }
+}
+
+class Button {
+    private List<ButtonListener> listeners;
+
+    public Button() {
+        this.listeners = new LinkedList<ButtonListener>();
+    }
+
+    public void addListener(ButtonListener listener) {
+        assert listener != null;
+        listeners.add(listener);
+    }
+
+    public void press() {
+        for (ButtonListener listener : listeners) {
+            listener.buttonPressed();
+        }
+    }
+}
+
+class ButtonListener {
+    void buttonPressed() {
+
+    }
+}
+
+class Dialer {
+    public void enterDigit(int digit) {
+        System.out.println("enter digit: " + digit);
+    }
+
+    public void dial() {
+        System.out.println("dialing...");
+    }
+}
