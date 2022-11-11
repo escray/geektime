3eeda12f9bf5f31b6cd49cc9c019425fcdb19b71
escray <zescray@gmail.com>
2022-11-11 16:37:58 +0800

    mybatis generator init

---
+++ spring-family-practice/chapter03/mybatis-generator-demo/src/test/java/geektime/spring/data/mybatisdemo/MybatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package geektime.spring.data.mybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
