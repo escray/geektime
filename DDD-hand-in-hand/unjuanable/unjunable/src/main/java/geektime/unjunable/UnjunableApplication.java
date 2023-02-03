af661aaa2018c7e9465d5790ed77468c09b821ba
escray <zescray@gmail.com>
2023-02-03 23:44:20 +0800

    initial code

---
+++ DDD-hand-in-hand/unjuanable/unjunable/src/main/java/geektime/unjunable/UnjunableApplication.java
@@ -0,0 +1,13 @@
+package geektime.unjunable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnjunableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnjunableApplication.class, args);
+    }
+
+}
---
+++ DDD-hand-in-hand/unjuanable/unjunable/src/test/java/geektime/unjunable/UnjunableApplicationTests.java
@@ -0,0 +1,13 @@
+package geektime.unjunable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnjunableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
